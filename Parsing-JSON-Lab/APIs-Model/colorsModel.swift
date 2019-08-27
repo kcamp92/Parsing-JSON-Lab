6fe64a2b94f881c601b68052a33848bc397f8384
Krystal Campbell <krystalcampbell@Krystals-MacBook-Pro.local>
2019-08-27 16:57:06 -0400

    added all the tableviews/cells to storyboard, uploaded all APIs

---
+++ Parsing-JSON-Lab/APIs-Model/colorsModel.swift
@@ -0,0 +1,9 @@
+//
+//  colorsModel.swift
+//  Parsing-JSON-Lab
+//
+//  Created by Krystal Campbell on 8/27/19.
+//  Copyright © 2019 Krystal Campbell. All rights reserved.
+//
+
+import Foundation
